3ab7374d440cdc1e40e312b7ec33fae58ce9343d
cookie-cutter-123 <acc@loncarevic.net>
2024-10-28 14:52:07 +0100

    Add recursive and annonymous functions

--- kode-kloud-go/main.go
+++ kode-kloud-go/main.go
@@ -195,9 +195,24 @@ func functionsPractice() {
 
 	fmt.Println(sumOfNumbers(1, 2, 3, 4, 5))
 	printDetails("Igor", "Math", "Science", "English")
+
+	fmt.Println(factorial(5))
+
+	// Anonymous function - generally used when you need a function for a short period of time
+	anonymousFunction1 := func(l int, b int) int {
+		return l * b
+	}
+	fmt.Printf("Datatype of x is %T\n", anonymousFunction1)
+	fmt.Println(anonymousFunction1(10, 5))
+
+	// Define anonymous function and call it directly
+	anonymousFunction2 := func(l int, b int) int {
+		return l * b
+	}(10, 5)
+	fmt.Println(anonymousFunction2)
 }
 
-func operation(a int, b int) (sum int, diff int)  {
+func operation(a int, b int) (sum int, diff int) {
 	sum = a + b
 	diff = a - b
 	return // Return statement without arguments
@@ -218,4 +233,12 @@ func printDetails(student string, subjects ...string) {
 	for _, subject := range subjects {
 		fmt.Println(subject)
 	}
+}
+
+// Recursive function
+func factorial(n int) int {
+	if n == 0 {
+		return 1
+	}
+	return n * factorial(n-1)
 }
