fe91e545eb1564a76d7a04d60c48f8005ccf5a57
cookie-cutter-123 <acc@loncarevic.net>
2024-11-20 17:31:10 +0100

    More pointers

--- kode-kloud-go/3_pointers.go
+++ kode-kloud-go/3_pointers.go
@@ -13,4 +13,32 @@ func pointersPractice() {
 
 	var ptrNumber10 *int = &number10
 	fmt.Printf("%T %v \n", ptrNumber10, ptrNumber10)
+
+	// Dereferencing a pointer
+	*ptrNumber10 = 20
+	fmt.Printf("%T %v \n", number10, number10)
+
+	// Passing by value does not change the original value
+	fmt.Println("\nPassing by value:")
+	hello := "Hello"
+	fmt.Println("Before: ", hello)
+	modify(hello)
+	fmt.Println("After: ", hello)
+
+	// Passing by reference changes the original value since we are passing the memory address
+	fmt.Println("\nPassing by reference:")
+	fmt.Println("Before: ", hello)
+	modifyPtr(&hello)
+	fmt.Println("After: ", hello)
+
+	// Slices are passed by reference by default since they are pointers to an array
+	// Maps are also passed by reference
+}
+
+func modify(hello string) {
+	hello += " world"
+}
+
+func modifyPtr(hello *string) {
+	*hello += " world"
 }
