72b78045f3b53dbbdae7767dd6ffaa76eb292ac2
cookie-cutter-123 <acc@loncarevic.net>
2024-12-06 23:36:36 +0100

    More structs

--- kode-kloud-go/4_structs.go
+++ kode-kloud-go/4_structs.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 /* Structs:
 - user-defined data types
 - a structure that groups together data elements
@@ -8,5 +10,42 @@ package main
  */
 
 func structsPractice(){
+	type Student struct {
+		name string
+		rollNo int
+		marks []int
+		grades map[string]int
+	}
 
+	var s Student
+	fmt.Printf("%+v\n", s)
+
+	igor := Student{
+		name: "Igor",
+		rollNo: 12,
+	}
+
+	fmt.Printf("%+v\n", igor)
+	fmt.Println(igor.name)
+	igor.rollNo = 13 // update value
+	fmt.Printf("%+v\n", igor)
+
+	c := Circle{x: 5, y: 5, radius: 5, area: 0}
+	fmt.Printf("%+v\n", c)
+	calculateArea(&c)
+	fmt.Printf("%+v\n", c)
+}
+
+type Circle struct {
+	x int
+	y int
+	radius float64
+	area float64
+}
+
+func calculateArea(c *Circle) {
+	const PI = 3.14
+	var area float64
+	area = PI * c.radius * c.radius
+	(*c).area = area
 }
